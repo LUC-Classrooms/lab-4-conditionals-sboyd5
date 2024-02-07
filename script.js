5cf79b49e7e0847136968446d1e1875ff8cb865d
sboyd5 <157087132+sboyd5@users.noreply.github.com>
2024-02-07 00:45:54 +0000

    Lab 4 - Submitted

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+var x, w:
+
 function setup() {
   createCanvas(320, 240);
+  x = width/2;
+  w = 100;
 }
 
 function draw() {
@@ -8,11 +12,15 @@ function draw() {
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    
+    Fill(255, 0, 0);
+    x = width/4;
+    w = 50;
   }
   else { 
     // otherwise do this:
-    
+    fill(0, 255, 0);
+    x = width/2;
+    w = 100;
   }
-  ellipse(width/2, height/2, 100, 100);
+  ellipse(x, height/2, w, 100);
 }
